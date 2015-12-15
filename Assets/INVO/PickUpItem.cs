d4ffa7f106e8d66ce489e76350f9859994578e90
Zack Rock <zacharyrock@hotmail.com>
2015-12-14 18:41:23 -0800

    Left Panel spawning objects.

--- Assets/INVO/PickUpItem.cs
+++ Assets/INVO/PickUpItem.cs
@@ -4,4 +4,12 @@ using System.Collections;
 public class PickUpItem : MonoBehaviour {
 	public string itemName; 
 	public Sprite image;
+
+	void OnTriggerEnter(Collider col) {
+		if(col.tag == "Player") {
+			transform.parent = null;
+			GetComponent<Collider>().isTrigger = false;
+			GetComponent<Rigidbody>().isKinematic = false;
+		}
+	}
 }
---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject : MonoBehaviour {
+
+	public GameObject objectHolder;
+	public GameObject objectToSpawn;
+
+//	void Start () {
+//	
+//	}
+//	
+//	void Update () {
+//	
+//	}
+
+	void Activate() {
+		if(objectHolder.transform.childCount == 0) {
+			GameObject obj = (GameObject)Instantiate(objectToSpawn, objectHolder.transform.position + Vector3.up, objectHolder.transform.rotation);
+			obj.transform.parent = objectHolder.transform;
+		}
+	}
+}
